29b6a41b5d76e6efea52c3b28639bec47bb32bd9
IAGO LEAL DE MATTOS <iago.mattos@aluno.senac.rio>
2025-04-29 20:40:44 -0300

    mudanças no exemplo 01

--- codigo/exemplo01.py
+++ codigo/exemplo01.py
@@ -5,78 +5,78 @@
 
 ############################################
 
-# def saudacao():
-#     print("Olá Mundo")
+def saudacao():
+    print("Olá Mundo")
 
 
 # Chama a função
-# saudacao()
+saudacao()
 ############################################
 
-# def linha():
-#     print("-" * 30)
+def linha():
+    print("-" * 30)
 
 
-# linha()
-# print("MODULO 01")
-# linha()
-# print("ALGORITMOS")
-# linha()
-# print("ANALISE DE DADOS")
-# linha()
-
-##########################################
-
-# def saudacao(nome):
-#     print(f"Olá {nome}!")
-
-
-# nome = input("Digite seu nome: ")
-# saudacao(nome)
+linha()
+print("MODULO 01")
+linha()
+print("ALGORITMOS")
+linha()
+print("ANALISE DE DADOS")
+linha()
 
 ##########################################
 
-# def soma(n1, n2):
-#     somar = n1 + n2
-#     print(f"A soma de {n1} + {n2} é igual a {somar}")
+def saudacao(nome):
+    print(f"Olá {nome}!")
 
 
-# def subtracao(n1, n2):
-#     subtrair = n1 - n2
-#     print(f"A subtração de {n1} - {n2} é igual a {subtrair}")
+nome = input("Digite seu nome: ")
+saudacao(nome)
+
+##########################################
+
+def soma(n1, n2):
+    somar = n1 + n2
+    print(f"A soma de {n1} + {n2} é igual a {somar}")
 
 
-# def multiplicacao(n1, n2):
-#     multiplicar = n1 * n2
-#     print(f"A multiplicação de {n1} * {n2} é igual a {multiplicar}")
+def subtracao(n1, n2):
+    subtrair = n1 - n2
+    print(f"A subtração de {n1} - {n2} é igual a {subtrair}")
 
 
-# def divisao(n1, n2):
-#     if n2 == 0:
-#         print("Não é possível dividir por zero")
-#     else:
-#         dividir = n1 / n2
-#         print(f"A divisão de {n1} / {n2} é igual a {dividir}")
+def multiplicacao(n1, n2):
+    multiplicar = n1 * n2
+    print(f"A multiplicação de {n1} * {n2} é igual a {multiplicar}")
 
 
-# n1 = int(input("Digite o primeiro número: "))
-# n2 = int(input("Digite o segundo número: "))
+def divisao(n1, n2):
+    if n2 == 0:
+        print("Não é possível dividir por zero")
+    else:
+        dividir = n1 / n2
+        print(f"A divisão de {n1} / {n2} é igual a {dividir}")
 
-# soma(n1, n2)
-# subtracao(n1, n2)
-# multiplicacao(n1, n2)
-# divisao(n1, n2)
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+soma(n1, n2)
+subtracao(n1, n2)
+multiplicacao(n1, n2)
+divisao(n1, n2)
 
 ###########################################
 
-# def somar_numeros(x, y):
-#     s = x + y
-#     return s
+def somar_numeros(x, y):
+    s = x + y
+    return s
 
 
-# for i in range(3):
-#     n1 = int(input("Digite o primeiro número: "))
-#     n2 = int(input("Digite o segundo número: "))
+for i in range(3):
+    n1 = int(input("Digite o primeiro número: "))
+    n2 = int(input("Digite o segundo número: "))
 
-# soma = somar_numeros(n1, n2)
-# print(soma)
+soma = somar_numeros(n1, n2)
+print(soma)
